5fd93ec257e2669c44b74817477994091e660f3b
Max Peshkov <maxpeshkovit@gmail.com>
2025-09-28 14:04:37 +0000

    Implement Exercise 2.2

---
+++ ch2/arith.h
@@ -0,0 +1,6 @@
+int Arith_max(int x, int y);
+int Arith_min(int x, int y);
+int Arith_div(int x, int y);
+int Arith_mod(int x, int y);
+int Arith_ceiling(int x, int y);
+int Arith_floor(int x, int y);
