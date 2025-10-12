5e86702e9b6a1bea905a89812af1a6c674282c35
Max Peshkov <maxpeshkovit@gmail.com>
2025-10-12 22:39:27 +0400

    Exercise 2.4

--- ch2/stack.c
+++ ch2/stack.c
@@ -1,10 +1,12 @@
 #include <stddef.h>
+#include <stdint.h>
 #include "assert.h"
 #include "mem.h"
 #include "stack.h"
 
 #define T Stack_T
 #define MAGIC_VALUE 0x00008123
+#define isBadPtr(p) ((p) == NULL || ((uintptr_t)((void *)(p))) % 8 != 0)
 
 struct T {
 	unsigned count;
@@ -26,14 +28,14 @@ T Stack_new(void) {
 }
 
 int Stack_empty(T stk) {
-	assert(stk);
+	assert(!isBadPtr(stk));
 	return stk->count == 0;
 }
 
 void Stack_push(T stk, void *x) {
 	struct elem *t;
 
-	assert(stk);
+	assert(!isBadPtr(stk));
 	NEW(t);
 	t->x = x;
 	t->link = stk->head;
@@ -45,7 +47,7 @@ void *Stack_pop(T stk) {
 	void *x;
 	struct elem *t;
 
-	assert(stk);
+	assert(!isBadPtr(stk));
 	assert(stk->count > 0);
 	t = stk->head;
 	stk->head = t->link;
@@ -58,7 +60,8 @@ void *Stack_pop(T stk) {
 void Stack_free(T *stk) {
 	struct elem *t, *u;
 
-	assert(stk && *stk);
+	assert(!isBadPtr(stk));
+	assert(!isBadPtr(*stk));
 	for (t = (*stk)->head; t; t = u) {
 		u = t->link;
 		FREE(t);
